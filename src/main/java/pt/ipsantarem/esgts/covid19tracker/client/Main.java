88fc8911d83f9605856dd3d2ccc326d623eda79f
José Simões <jose.etpr99@gmail.com>
2020-05-12 16:19:54 +0100

    First commit of the project. Further work pending.

---
+++ src/main/java/pt/ipsantarem/esgts/covid19tracker/client/Main.java
@@ -0,0 +1,15 @@
+package pt.ipsantarem.esgts.covid19tracker.client;
+
+import static def.dom.Globals.alert;
+
+/**
+ * Entry point for the client-side component.
+ * Do not run the main method directly! The code in the client package is supposed to be compiled by the JSweet Maven
+ * plugin.
+ * BE CAREFUL WITH USING {@link java} CLASSES HERE!
+ */
+public class Main {
+    public static void main(String[] args) {
+        alert("Teste!");
+    }
+}
---
+++ src/main/java/pt/ipsantarem/esgts/covid19tracker/server/Main.java
@@ -0,0 +1,10 @@
+package pt.ipsantarem.esgts.covid19tracker.server;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
